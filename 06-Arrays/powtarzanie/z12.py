e9a838483e1150aa25946c458752bbe8ac31c567
Uliana4 <227397@student.uek.krakow.pl>
2023-12-06 19:10:31 +0100

    UK

--- 06-Arrays/powtarzanie/z12.py
+++ 06-Arrays/powtarzanie/z12.py
@@ -2,5 +2,5 @@ arr = [34,7,19,4,21,8]
 even = 0
 for a in arr:
     if a%2==0:
-        even = even + 1
+        even += 1
 print(even)
--- 06-Arrays/powtarzanie/z26.py
+++ 06-Arrays/powtarzanie/z26.py
@@ -1,8 +1,3 @@
 arr = ["Genowefa", "Onufry", "Celestyna", "Alojzy", "Pankracy"]
-# for i in range(len(arr)):
-#     str_i = str(i)
-#     if len(str_i)<len(i+1):
-#         big = arr[i+1]
-# print(big)
 longest_name = max(arr, key=len)
 print(longest_name)
